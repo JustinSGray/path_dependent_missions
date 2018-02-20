bd9164e18019d9767b93b5e38168a2ac63f1f33c
johnjasa <johnjasa11@gmail.com>
2018-02-20 15:30:06 -0500

    Tuned SMT model

--- path_dependent_missions/F110/smt_model.py
+++ path_dependent_missions/F110/smt_model.py
@@ -2,63 +2,74 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pickle
 import os
-from smt.surrogate_models import RMTB
+from smt.surrogate_models import RMTB, RMTC
 
-# MN,  Alt,  PC,  Fg (total gross thrust, lbf),  Ram (ram drag, lbf),  Fueltot (lbm/sec)
-data = np.loadtxt('good_output_flops')
 
-print(data.shape)
-xt = data[:, :3]
-yt = data[:, 3:]
+def get_data():
+    # MN,  Alt,  PC,  Fg (total gross thrust, lbf),  Ram (ram drag, lbf),  Fueltot (lbm/sec)
+    data = np.loadtxt('good_output_flops')
 
-# Scale inputs
-xt[:, 1] /= 1e4
-xt[:, 2] /= 1e2
+    xt = data[:, :3]
+    net_thrust = data[:, 3] - data[:, 4]
+    yt = np.vstack((net_thrust, data[:, 5])).T
 
-# Scale outputs
-yt[:, 0] /= 1e4
-yt[:, 1] /= 1e4
-yt[:, 2] /= 1e4
+    # Scale inputs
+    xt[:, 1] /= 1e4
+    xt[:, 2] /= 1e2
 
-xlimits = np.array([
-    [0.0, 1.8],
-    [0., 7.],
-    [-.01, 1.01],
-])
+    # Scale outputs
+    yt[:, 0] /= 1e4
+    yt[:, 1] /= 1e4
 
-interp = RMTB(xlimits=xlimits, num_ctrl_pts=15, order=4,
-    approx_order=2, nonlinear_maxiter=0, solver_tolerance=1.e-20,
-    # solver='lu', derivative_solver='lu',
-    energy_weight=1.e-4, regularization_weight=0.e-20, extrapolate=False, print_global=True,
-    data_dir='_smt_cache/',
-)
+    xlimits = np.array([
+        [0.0, 1.8],
+        [0., 7.],
+        [-.01, 1.01],
+    ])
 
-interp.set_training_values(xt, yt)
-interp.train()
+    return xt, yt, xlimits
 
-from postprocessing.MultiView.MultiView import MultiView
-import pickle
-import scipy.interpolate
+def get_F110_interp():
 
-info = {'nx':3,
-        'ny':3,
-        'user_func':interp.predict_values,
-        'resolution':60,
-        'plot_size':8,
-        'dimension_names':[
-            'Mach number',
-            'Altitude',
-            'Throttle'],
-        'bounds':xlimits.tolist(),
-        'X_dimension':0,
-        'Y_dimension':1,
-        'scatter_points':[xt, yt],
-        'dist_range': .1,
-        'output_names':[
-            'Thrust',
-            'Drag',
-            'Fuelburn',
-        ]}
+    xt, yt, xlimits = get_data()
 
-# Initialize display parameters and draw GUI
-MultiView(info)
+    interp = RMTB(xlimits=xlimits, num_ctrl_pts=15, order=4,
+        approx_order=2, nonlinear_maxiter=40, solver_tolerance=1.e-20,
+        # solver='lu', derivative_solver='lu',
+        energy_weight=1.e-4, regularization_weight=0.e-18, extrapolate=False, print_global=True,
+        data_dir='_smt_cache/',
+    )
+
+    interp.set_training_values(xt, yt)
+    interp.train()
+
+    return interp
+
+
+if __name__ == "__main__":
+    from postprocessing.MultiView.MultiView import MultiView
+
+    xt, yt, xlimits = get_data()
+    interp = get_F110_interp()
+
+    info = {'nx':3,
+            'ny':2,
+            'user_func':interp.predict_values,
+            'resolution':90,
+            'plot_size':10,
+            'dimension_names':[
+                'Mach number',
+                'Altitude',
+                'Throttle'],
+            'bounds':xlimits.tolist(),
+            'X_dimension':0,
+            'Y_dimension':1,
+            'scatter_points':[xt, yt],
+            'dist_range': .1,
+            'output_names':[
+                'Net Thrust, 10k lbf',
+                'Fuelburn, 10k lbm/sec',
+            ]}
+
+    # Initialize display parameters and draw GUI
+    MultiView(info)
