c435a604d39c20846aa420af2e2a1ba2086fd857
johnjasa <johnjasa11@gmail.com>
2018-02-22 17:32:25 -0500

    Added full escort mission; not getting good  results

--- path_dependent_missions/escort/full_escort_problem.py
+++ path_dependent_missions/escort/full_escort_problem.py
@@ -22,7 +22,7 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
     p.driver = pyOptSparseDriver()
     p.driver.options['optimizer'] = optimizer
     if optimizer == 'SNOPT':
-        p.driver.opt_settings['Major iterations limit'] = 1000
+        p.driver.opt_settings['Major iterations limit'] = 200
         p.driver.opt_settings['Iterations limit'] = 100000000
         p.driver.opt_settings['iSumm'] = 6
         p.driver.opt_settings['Major feasibility tolerance'] = 1.0E-6
@@ -79,16 +79,13 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
     climb.add_path_constraint(name='h', lower=100.0, upper=20000, ref=20000)
     climb.add_path_constraint(name='aero.mach', lower=0.1, upper=1.8)
 
-    # Minimize time at the end of the climb
-    # climb.set_objective('time', loc='final', ref=100.0)
-
     p.model.add_subsystem('climb', climb)
 
 
 
 
     escort = phase_class(ode_function=MinTimeClimbODE(),
-                        num_segments=num_seg*2,
+                        num_segments=num_seg,
                         transcription_order=transcription_order,
                         compressed=False)
 
@@ -118,12 +115,6 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
     escort.add_control('throttle', val=1.0, lower=0., upper=1., dynamic=True, opt=True)
     # escort.add_control('throttle', val=1.0, dynamic=False, opt=False)
 
-    # escort.add_boundary_constraint('h', loc='final', equals=20000, scaler=1.0E-3, units='m')
-    # escort.add_boundary_constraint('aero.mach', loc='final', equals=1.0, units=None)
-    # escort.add_boundary_constraint('gam', loc='final', equals=0.0, units='rad')
-    escort.add_boundary_constraint('m', loc='final', equals=15000.0, units='kg')
-
-    #
     escort.add_path_constraint(name='h', lower=meeting_altitude, upper=meeting_altitude, ref=meeting_altitude)
     escort.add_path_constraint(name='aero.mach', equals=1.0)
 
@@ -131,6 +122,49 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
     escort.set_objective('r', loc='final', ref=-1e5)
 
     p.model.add_subsystem('escort', escort)
+
+
+
+
+
+    descent = phase_class(ode_function=MinTimeClimbODE(),
+                        num_segments=num_seg,
+                        transcription_order=transcription_order,
+                        compressed=False)
+
+    descent.set_time_options(duration_bounds=(10, 100), duration_ref=100.0)
+
+    descent.set_state_options('r', lower=0, upper=1.0E6,
+                            scaler=1.0E-3, defect_scaler=1.0E-2, units='m')
+
+    descent.set_state_options('h', lower=0, upper=20000.0,
+                            scaler=1.0E-3, defect_scaler=1.0E-3, units='m')
+
+    descent.set_state_options('v', lower=10.0,
+                            scaler=1.0E-2, defect_scaler=1.0E-2, units='m/s')
+
+    descent.set_state_options('gam', lower=-1.5, upper=1.5,
+                            ref=1.0, defect_scaler=1.0, units='rad')
+
+    descent.set_state_options('m', lower=10.0, upper=1.0E5,
+                            scaler=1.0E-3, defect_scaler=1.0E-3)
+
+    descent.add_control('alpha', units='deg', lower=-8.0, upper=8.0, scaler=1.0,
+                      dynamic=True, rate_continuity=True)
+
+    descent.add_control('S', val=49.2386, units='m**2', dynamic=False, opt=False)
+    descent.add_control('Isp', val=1600.0, units='s', dynamic=False, opt=False)
+
+    # descent.add_control('throttle', val=1.0, lower=0., upper=1., dynamic=True, opt=True)
+    descent.add_control('throttle', val=0., dynamic=False, opt=False)
+
+    descent.add_boundary_constraint('m', loc='final', equals=14000.0, units='kg')
+    descent.add_boundary_constraint('h', loc='final', equals=100.0, units='m')
+    descent.add_path_constraint(name='aero.mach', upper=1.2)
+
+    # descent.set_objective('time', loc='final', ref=1e2)
+
+    p.model.add_subsystem('descent', descent)
 
 
 
@@ -172,6 +206,43 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
 
     p.model.add_subsystem('linkages', linkage_comp)
 
+    # Connect the phases
+    linkage_comp = PhaseLinkageComp()
+    linkage_comp.add_linkage(name='L01', vars=['t'], units='s', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['r'], units='m', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['h'], units='m', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['v'], units='m/s', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['gam'], units='rad', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['m'], units='kg', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['alpha'], units='rad', equals=0.0, linear=True)
+    linkage_comp.add_linkage(name='L01', vars=['throttle'], equals=0.0, linear=True)
+
+    p.model.connect('escort.time++', 'linkages_2.L01_t:lhs')
+    p.model.connect('descent.time--', 'linkages_2.L01_t:rhs')
+
+    p.model.connect('escort.states:r++', 'linkages_2.L01_r:lhs')
+    p.model.connect('descent.states:r--', 'linkages_2.L01_r:rhs')
+
+    p.model.connect('escort.states:h++', 'linkages_2.L01_h:lhs')
+    p.model.connect('descent.states:h--', 'linkages_2.L01_h:rhs')
+    #
+    p.model.connect('escort.states:v++', 'linkages_2.L01_v:lhs')
+    p.model.connect('descent.states:v--', 'linkages_2.L01_v:rhs')
+    #
+    p.model.connect('escort.states:gam++', 'linkages_2.L01_gam:lhs')
+    p.model.connect('descent.states:gam--', 'linkages_2.L01_gam:rhs')
+
+    p.model.connect('escort.states:m++', 'linkages_2.L01_m:lhs')
+    p.model.connect('descent.states:m--', 'linkages_2.L01_m:rhs')
+
+    p.model.connect('escort.controls:alpha++', 'linkages_2.L01_alpha:lhs')
+    p.model.connect('descent.controls:alpha--', 'linkages_2.L01_alpha:rhs')
+
+    p.model.connect('escort.controls:throttle++', 'linkages_2.L01_throttle:lhs')
+    p.model.connect('descent.controls:throttle--', 'linkages_2.L01_throttle:rhs')
+
+    p.model.add_subsystem('linkages_2', linkage_comp)
+
 
 
 
@@ -194,11 +265,20 @@ def escort_problem(optimizer='SLSQP', num_seg=3, transcription_order=5,
     p['escort.t_initial'] = 300.
     p['escort.t_duration'] = 1000.
     p['escort.states:r'] = escort.interpolate(ys=[111319.54, 400000.], nodes='disc')
-    p['escort.states:h'] = escort.interpolate(ys=[meeting_altitude, meeting_altitude], nodes='disc')
+    p['escort.states:h'] = escort.interpolate(ys=[20000., 20000.0], nodes='disc')
     p['escort.states:v'] = escort.interpolate(ys=[250., 250.], nodes='disc')
     p['escort.states:gam'] = escort.interpolate(ys=[0.0, 0.0], nodes='disc')
     p['escort.states:m'] = escort.interpolate(ys=[16841.431, 15000.], nodes='disc')
     p['escort.controls:alpha'] = escort.interpolate(ys=[0.0, 0.0], nodes='all')
+
+    p['descent.t_initial'] = 1000.
+    p['descent.t_duration'] = 1100.
+    p['descent.states:r'] = descent.interpolate(ys=[400000., 500000.], nodes='disc')
+    p['descent.states:h'] = descent.interpolate(ys=[20000., 100.], nodes='disc')
+    p['descent.states:v'] = descent.interpolate(ys=[283.159, 135.], nodes='disc')
+    p['descent.states:gam'] = descent.interpolate(ys=[0.0, 0.0], nodes='disc')
+    p['descent.states:m'] = descent.interpolate(ys=[15000., 14500.], nodes='disc')
+    p['descent.controls:alpha'] = descent.interpolate(ys=[0.0, 0.0], nodes='all')
 
     return p
 
@@ -220,7 +300,7 @@ if __name__ == '__main__':
     colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
     f, axarr = plt.subplots(6, sharex=True)
 
-    for i, phase in enumerate([p.model.climb, p.model.escort]):
+    for i, phase in enumerate([p.model.climb, p.model.escort, p.model.descent]):
 
         time = phase.get_values('time', nodes='all')
         m = phase.get_values('m', nodes='all')
@@ -250,7 +330,7 @@ if __name__ == '__main__':
         axarr[5].set_ylabel('throttle')
         axarr[5].set_xlabel('range')
 
-        exp_out = phase.simulate(times=np.linspace(0, p['climb.t_duration'], 50))
+        exp_out = phase.simulate(times=np.linspace(0, p['climb.t_duration'], 100))
         time2 = exp_out.get_values('time')
         m2 = exp_out.get_values('m')
         mach2 = exp_out.get_values('aero.mach')
