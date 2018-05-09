2e27fcd0213f0bec2e57cd14b12c9ccc7fc1b2dc
John Jasa <johnjasa@umich.edu>
2018-05-08 22:34:55 -0400

    Added saving and plotting ability for thermal mission cases

--- path_dependent_missions/thermal_mission/thermal_mission_problem.py
+++ path_dependent_missions/thermal_mission/thermal_mission_problem.py
@@ -4,21 +4,21 @@ import matplotlib
 import numpy as np
 
 from openmdao.api import Problem, Group, pyOptSparseDriver, DenseJacobian, DirectSolver, \
-    CSCJacobian, CSRJacobian
+    CSCJacobian, CSRJacobian, SqliteRecorder
 
 from dymos import Phase
 
 from thermal_mission_ode import ThermalMissionODE
-from path_dependent_missions.utils.gen_mission_plot import plot_results
+from path_dependent_missions.utils.gen_mission_plot import save_results, plot_results
 
 
-def thermal_mission_problem(num_seg=5, transcription_order=3, meeting_altitude=20000., Q_env=0., Q_sink=0., Q_out=0., m_flow=0.1, opt_m_flow=False, opt_m_burn=False, engine_heat_coeff=0., pump_heat_coeff=0.):
+def thermal_mission_problem(num_seg=5, transcription_order=3, meeting_altitude=20000., Q_env=0., Q_sink=0., Q_out=0., m_flow=0.1, opt_m_flow=False, opt_m_burn=False, opt_throttle=True, engine_heat_coeff=0., pump_heat_coeff=0., record=True):
 
     p = Problem(model=Group())
 
     p.driver = pyOptSparseDriver()
     p.driver.options['optimizer'] = 'SNOPT'
-    p.driver.opt_settings['Major iterations limit'] = 500
+    p.driver.opt_settings['Major iterations limit'] = 5000
     p.driver.opt_settings['Iterations limit'] = 5000000000000000
     p.driver.opt_settings['iSumm'] = 6
     p.driver.opt_settings['Major feasibility tolerance'] = 1.0E-8
@@ -56,8 +56,10 @@ def thermal_mission_problem(num_seg=5, transcription_order=3, meeting_altitude=2
                       dynamic=True, rate_continuity=True)
 
     phase.add_control('S', val=49.2386, units='m**2', dynamic=False, opt=False)
-    phase.add_control('throttle', val=1.0, dynamic=False, opt=False)
-    # phase.add_control('throttle', val=1.0, lower=0.0, upper=1.0, dynamic=True, opt=True)
+    if opt_throttle:
+        phase.add_control('throttle', val=1.0, lower=0.0, upper=1.0, dynamic=True, opt=True, rate_continuity=True)
+    else:
+        phase.add_control('throttle', val=1.0, dynamic=False, opt=False)
     phase.add_control('W0', val=10000., dynamic=False, opt=False, units='kg')
 
     phase.add_boundary_constraint('h', loc='final', equals=meeting_altitude, scaler=1.0E-3, units='m')
@@ -94,32 +96,33 @@ def thermal_mission_problem(num_seg=5, transcription_order=3, meeting_altitude=2
         phase.add_path_constraint('T', lower=0.)
         phase.add_path_constraint('T', upper=310., ref=300.)
         phase.add_path_constraint('T_o', lower=0., units='K')
-        phase.add_path_constraint('T_o', upper=440., units='K', ref=300.)
+        # phase.add_path_constraint('T_o', upper=302., units='K', ref=300.)
         # # phase.add_path_constraint('m_flow_rate', upper=0.)
-        phase.add_path_constraint('m_recirculated', lower=0., units='kg/s', ref=10.)
-        # phase.add_path_constraint('m_flow', lower=0., upper=40., ref=20., units='kg/s')
+        phase.add_path_constraint('m_recirculated', lower=0., upper=0., units='kg/s', ref=10.)
+        phase.add_path_constraint('m_flow', lower=0., upper=40., ref=20., units='kg/s')
 
     p.setup(mode='fwd', check=True)
+    if record:
+        p.driver.add_recorder(SqliteRecorder('out.db'))
 
-    p['phase.t_initial'] = 0.0
-    p['phase.t_duration'] = 50.
-    p['phase.states:r'] = phase.interpolate(ys=[0.0, 111319.54], nodes='disc')
-    p['phase.states:h'] = phase.interpolate(ys=[100.0, meeting_altitude], nodes='disc')
-    p['phase.states:v'] = phase.interpolate(ys=[135.964, 283.159], nodes='disc')
-    p['phase.states:gam'] = phase.interpolate(ys=[0.0, 0.0], nodes='disc')
-    p['phase.states:m'] = phase.interpolate(ys=[20.e3, 16841.431], nodes='disc')
-    # p['phase.controls:alpha'] = phase.interpolate(ys=[0.50, 0.50], nodes='all')
+        p['phase.t_initial'] = 0.0
+        p['phase.t_duration'] = 50.
+        p['phase.states:r'] = phase.interpolate(ys=[0.0, 111319.54], nodes='disc')
+        p['phase.states:h'] = phase.interpolate(ys=[100.0, meeting_altitude], nodes='disc')
+        p['phase.states:v'] = phase.interpolate(ys=[135.964, 283.159], nodes='disc')
+        p['phase.states:gam'] = phase.interpolate(ys=[0.0, 0.0], nodes='disc')
+        p['phase.states:m'] = phase.interpolate(ys=[20.e3, 16841.431], nodes='disc')
+        # p['phase.controls:alpha'] = phase.interpolate(ys=[0.50, 0.50], nodes='all')
 
-    # Give initial values for the phase states, controls, and time
-    p['phase.states:T'] = 300.
+        # Give initial values for the phase states, controls, and time
+        p['phase.states:T'] = 300.
 
     return p
 
 
 if __name__ == '__main__':
-    p = thermal_mission_problem(num_seg=10, transcription_order=3, m_flow=30., opt_m_flow=True, Q_env=0.e3, Q_sink=100.e3, Q_out=0.e3, pump_heat_coeff=100.e3)
-    # p = thermal_mission_problem(num_seg=8, transcription_order=3, m_flow=0.1, opt_m_flow=True)
-    p.run_model()
+    p = thermal_mission_problem(num_seg=12, transcription_order=3, m_flow=30., opt_m_flow=True, Q_env=0.e3, Q_sink=100.e3, Q_out=0.e3, engine_heat_coeff=60.e3)
     p.run_driver()
 
-    plot_results(p, ['h', 'aero.mach', 'm_fuel', 'T', 'T_o', 'm_flow', 'm_burn', 'm_recirculated'])
+    save_results(p, 'test.pkl')
+    plot_results('test.pkl', save_fig=True)
--- path_dependent_missions/utils/gen_mission_plot.py
+++ path_dependent_missions/utils/gen_mission_plot.py
@@ -4,8 +4,51 @@ import sys
 import os
 from collections import OrderedDict
 import matplotlib.pyplot as plt
+import pickle
 
-def plot_results(p, list_to_plot=[], sim_plot=True):
+
+def save_results(p, filename, run_sim=True):
+    """
+    Helper function to perform explicit simulation at the optimized point
+    and save the results.
+
+    Parameters
+    ----------
+    p : OpenMDAO Problem instance
+        This problem instance should contain the optimized outputs from the
+        energy minimization thermal problem.
+    """
+
+    list_to_save =  ['h', 'aero.mach', 'm_fuel', 'T', 'T_o', 'm_flow', 'm_burn', 'm_recirculated', 'throttle']
+
+    if run_sim:
+        # This hides the print output of simulate
+        sys.stdout = open(os.devnull, "w")
+        out = p.model.phase.simulate(times=np.linspace(p['phase.t_initial'], p['phase.t_duration'], 100))
+        sys.stdout = sys.__stdout__
+
+    n_vars = len(list_to_save)
+
+    col_vals = OrderedDict()
+    sim_vals = OrderedDict()
+
+    col_vals['time'] = p.model.phase.get_values('time', nodes='all')
+    if run_sim:
+        sim_vals['time'] = out.get_values('time')
+    for i, name in enumerate(list_to_save):
+        col_vals[name] = p.model.phase.get_values(name, nodes='all')
+
+        if run_sim:
+            sim_vals[name] = out.get_values(name)
+
+    big_dict = {'col_vals' : col_vals,
+                'sim_vals' : sim_vals,
+                'run_sim'  : run_sim}
+
+    with open(filename, 'wb') as f:
+        pickle.dump(big_dict, f)
+
+def plot_results(filename, save_fig=False, list_to_plot=['h', 'aero.mach', 'm_fuel', 'T', 'T_o', 'm_flow', 'm_burn', 'm_recirculated', 'throttle'], lines=[]):
     """
     Helper function to perform explicit simulation at the optimized point
     and plot the results. Points are the collocation nodes and the solid
@@ -18,32 +61,36 @@ def plot_results(p, list_to_plot=[], sim_plot=True):
         energy minimization thermal problem.
     """
 
-    if sim_plot:
-        # This hides the print output of simulate
-        sys.stdout = open(os.devnull, "w")
-        out = p.model.phase.simulate(times=np.linspace(p['phase.t_initial'], p['phase.t_duration'], 100))
-        sys.stdout = sys.__stdout__
+    with open(filename, 'rb') as f:
+        big_dict = pickle.load(f)
+
+    col_vals = big_dict['col_vals']
+    sim_vals = big_dict['sim_vals']
+    sim_plot = big_dict['run_sim']
 
     n_vars = len(list_to_plot)
-    f, axarr = plt.subplots(n_vars, sharex=True)
+    f, axarr = plt.subplots(n_vars, sharex=True, figsize=(6, 10))
 
-    col_vals = OrderedDict()
-    sim_vals = OrderedDict()
-
-    col_vals['time'] = p.model.phase.get_values('time', nodes='all')
-    if sim_plot:
-        sim_vals['time'] = out.get_values('time')
     for i, name in enumerate(list_to_plot):
-        col_vals[name] = p.model.phase.get_values(name, nodes='all')
         axarr[i].scatter(col_vals['time'], col_vals[name])
 
         if sim_plot:
-            sim_vals[name] = out.get_values(name)
             axarr[i].plot(sim_vals['time'], sim_vals[name])
 
-        axarr[i].set_ylabel('mass, kg')
+        for line in lines:
+            if line[0] == name:
+                axarr[i].axhline(y=line[1], color='r')
+
         axarr[i].set_ylabel(name)
 
     axarr[-1].set_xlabel('time, sec')
 
-    plt.show()
+    plt.tight_layout()
+
+    if save_fig:
+        plt.savefig(filename.split('.')[0]+'.pdf')
+    else:
+        plt.show()
+
+if __name__ == "__main__":
+    plot_results('test.pkl', save_fig=False, lines=[['T_o', 302.]])
