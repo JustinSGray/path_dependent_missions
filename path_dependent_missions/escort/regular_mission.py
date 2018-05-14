Updated everything for correct units and scaling using the CFD data

path_dependent_missions/escort/regular_mission.py
@@ -9,7 +9,7 @@ from openmdao.api import Problem, Group, pyOptSparseDriver, DenseJacobian, Direc
 from dymos import Phase
 
 from min_time_climb_ode import MinTimeClimbODE
-from path_dependent_missions.utils.gen_mission_plot import plot_results
+from path_dependent_missions.utils.gen_mission_plot import save_results, plot_results
 
 
 def min_time_climb_problem(num_seg=3, transcription_order=5,
@@ -28,6 +28,7 @@ def min_time_climb_problem(num_seg=3, transcription_order=5,
     p.driver.opt_settings['Verify level'] = 1
     p.driver.opt_settings['Function precision'] = 1.0E-6
     p.driver.opt_settings['Linesearch tolerance'] = .1
+    p.driver.opt_settings['Major step limit'] = .1
     p.driver.options['dynamic_simul_derivs'] = True
     p.driver.options['dynamic_simul_derivs_repeats'] = 5
     # p.driver.options['debug_print'] = ['desvars', 'nl_cons', 'objs']
@@ -97,100 +98,8 @@ def min_time_climb_problem(num_seg=3, transcription_order=5,
 
 if __name__ == '__main__':
     p = min_time_climb_problem(transcription='gauss-lobatto', num_seg=30, transcription_order=3)
-    p.run_model()
     p.run_driver()
 
-    plot_results(p, ['h', 'm', 'r', 'alpha', 'gam', 'throttle', 'aero.mach', 'throttle_rate', 'throttle_rate2'])
-
-
-
-    phase = p.model.phase
-
-    # Plotting below here
-    import numpy as np
-    import matplotlib.pyplot as plt
-    # plt.switch_backend('agg')
-
-    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    f, axarr = plt.subplots(7, sharex=True)
-
-    time = phase.get_values('time', nodes='all')
-    m = phase.get_values('m', nodes='all')
-    mach = phase.get_values('aero.mach', nodes='all')
-    h = phase.get_values('h', nodes='all')
-    r = phase.get_values('r', nodes='all') / 1e3
-    alpha = phase.get_values('alpha', nodes='all')
-    gam = phase.get_values('gam', nodes='all')
-    throttle = phase.get_values('throttle', nodes='all')
-
-    pad = 90
-
-    axarr[0].plot(time, h, 'o', color=colors[0])
-    axarr[0].set_ylabel('altitude, m', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-    # axarr[0].set_yticks([0., 15000.])
-
-    axarr[1].plot(time, mach, 'o', color=colors[0])
-    axarr[1].set_ylabel('mach', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-
-    axarr[2].plot(time, m, 'o', color=colors[0])
-    axarr[2].set_ylabel('mass, kg', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-    # axarr[2].set_yticks([17695.2, 19000.])
-
-    axarr[3].plot(time, alpha, 'o', color=colors[0])
-    axarr[3].set_ylabel('alpha, deg', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-
-    axarr[4].plot(time, gam, 'o', color=colors[0])
-    axarr[4].set_ylabel('gamma', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-
-    axarr[5].plot(time, throttle, 'o', color=colors[0])
-    axarr[5].set_ylabel('throttle', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-
-    axarr[6].plot(time, r, 'o', color=colors[0])
-    axarr[6].set_ylabel('range', rotation='horizontal', horizontalalignment='left', labelpad=pad)
-
-    axarr[-1].set_xlabel('time, s')
-
-    exp_out = phase.simulate(times=np.linspace(0, p['phase.t_duration'], 100))
-    time2 = exp_out.get_values('time')
-    m2 = exp_out.get_values('m')
-    mach2 = exp_out.get_values('aero.mach')
-    h2 = exp_out.get_values('h')
-    r2 = exp_out.get_values('r') / 1e3
-    alpha2 = exp_out.get_values('alpha')
-    gam2 = exp_out.get_values('gam')
-    throttle2 = exp_out.get_values('throttle')
-    axarr[0].plot(time2, h2, color=colors[0])
-    axarr[1].plot(time2, mach2, color=colors[0])
-    axarr[2].plot(time2, m2, color=colors[0])
-    axarr[3].plot(time2, alpha2, color=colors[0])
-    axarr[4].plot(time2, gam2, color=colors[0])
-    axarr[5].plot(time2, throttle2, color=colors[0])
-    axarr[6].plot(time2, r2, color=colors[0])
-
-    # n_points = time.shape[0]
-    # col_data = np.zeros((n_points, 8))
-    # col_data[:, 0] = time[:, 0]
-    # col_data[:, 1] = m[:, 0]
-    # col_data[:, 2] = mach[:, 0]
-    # col_data[:, 3] = h[:, 0]
-    # col_data[:, 4] = r[:, 0]
-    # col_data[:, 5] = alpha[:, 0]
-    # col_data[:, 6] = gam[:, 0]
-    # col_data[:, 7] = throttle[:, 0]
-    # np.savetxt('col_data_4.dat', col_data)
-    #
-    # n_points = time2.shape[0]
-    # sim_data = np.zeros((n_points, 8))
-    # sim_data[:, 0] = time2[:, 0]
-    # sim_data[:, 1] = m2[:, 0]
-    # sim_data[:, 2] = mach2[:, 0]
-    # sim_data[:, 3] = h2[:, 0]
-    # sim_data[:, 4] = r2[:, 0]
-    # sim_data[:, 5] = alpha2[:, 0]
-    # sim_data[:, 6] = gam2[:, 0]
-    # sim_data[:, 7] = throttle2[:, 0]
-    # np.savetxt('sim_data_4.dat', sim_data)
-
-    plt.tight_layout()
-    # plt.savefig('min_time.pdf', bbox_inches='tight')
-    plt.show()
+    list_to_plot = ['h', 'm', 'r', 'alpha', 'gam', 'throttle', 'aero.mach', 'throttle_rate', 'throttle_rate2']
+    save_results(p, 'new.pkl', options={}, list_to_save=list_to_plot)
+    plot_results(['new.pkl'], save_fig=False, list_to_plot=list_to_plot)

path_dependent_missions/utils/compare_aero.py
@@ -24,7 +24,7 @@ def setup_prob(nn, h, v):
     ivc = IndepVarComp()
     ivc.add_output('h', shape=nn, val=h, units='ft')
     ivc.add_output('v', shape=nn, val=v, units='m/s')
-    ivc.add_output('alpha', shape=nn, val=np.linspace(-2., 8., nn), units='deg')
+    ivc.add_output('alpha', shape=nn, val=np.linspace(-8., 8., nn), units='deg')
     ivc.add_output('S', shape=nn, val=49.236, units='m**2')
 
     p.model.add_subsystem('ivc', ivc, promotes=['*'])

run_scripts/run_q_engine_comparisons.py
@@ -13,35 +13,36 @@ from path_dependent_missions.utils.gen_mission_plot import save_results, plot_re
 
 options = {
     'transcription' : 'gauss-lobatto',
-    'num_seg' : 12,
+    'num_seg' : 20,
     'transcription_order' : 3,
     'm_recirculated' : 0.,
     'opt_m_recirculated' : False,
-    'Q_env' : 100.e3,
     'Q_sink' : 0.e3,
     'Q_out' : 0.e3,
-    'T' : 315.,
-    # 'T_o' : 325,
-    'm_initial' : 40.e3,
+    'T' : 312.,
+    # 'pump_heat_coeff' : 50.e3,
+    # 'T_o' : 330.,
+    'm_initial' : 30.e3,
     'opt_throttle' : True,
     'opt_m' : True,
+    'engine_heat_coeff' : 0.,
     }
 
-coeff_list = np.linspace(6., 9., 3)*1e5
+Q_env_list = np.linspace(4., 5., 3)*1e5
 
-for i, engine_heat_coeff in enumerate(coeff_list):
-    options['engine_heat_coeff'] = engine_heat_coeff
+for i, Q_env in enumerate(Q_env_list):
+    options['Q_env'] = Q_env
     p = thermal_mission_problem(**options)
     p.run_driver()
-    save_results(p, 'engine_heat_coeff_{}.pkl'.format(i), options)
+    save_results(p, 'Q_env_{}.pkl'.format(i), options)
 
-plot_list = ['engine_heat_coeff_{}.pkl'.format(i) for i in range(len(coeff_list))]
+plot_list = ['Q_env_{}.pkl'.format(i) for i in range(len(Q_env_list))]
 f, axarr = plot_results(plot_list, save_fig=True, list_to_plot=['h', 'aero.mach', 'm_fuel', 'T', 'm_burn', 'throttle'], figsize=(8, 8))
 
 axarr[0].annotate('', xy=(.75, .25), xytext=(.55, .75), xycoords='axes fraction',
         arrowprops=dict(arrowstyle='->, head_width=.25', facecolor='gray'))
-axarr[0].annotate('increasing Q_engine', xy=(.5, .5), xytext=(.44, .82), xycoords='axes fraction', rotation=0.)
+axarr[0].annotate('increasing Q_env', xy=(.5, .5), xytext=(.44, .82), xycoords='axes fraction', rotation=0.)
 
 import matplotlib.pyplot as plt
-# plt.show()
-plt.savefig('engine_coeff_compare.pdf')
+plt.show()
+# plt.savefig('engine_coeff_compare.pdf')